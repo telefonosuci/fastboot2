639a666d83677e956866a1501d7615798c122229
Enrico <telefonosuci@gmail.com>
2016-02-22 22:04:05 +0100

    Edited shell script

--- bin/fastboot.sh
+++ bin/fastboot.sh
@@ -11,30 +11,30 @@ cd "$APP_DIR"
 if [ "$#" -ne 1 ]; then
 
   echo "****************************************************************************************************"
-  echo "*  Fastrack Usage                                                                                  *"
+  echo "*  fastboot Usage                                                                                  *"
   echo "*                                                                                                  *"
   echo "*  Parameters:                                                                                     *"
   echo "*                                                                                                  *"
-  echo "*  ./fastrack.sh -start     : Starts the application                                               *"
-  echo "*  ./fastrack.sh -stats     : Get application state and queues list                                *"
-  echo "*  ./fastrack.sh -refresh   : Reload the application configuration from SAP and restart the queue  *"
-  echo "*  ./fastrack.sh -version   : Get application version                                              *"
-  echo "*  ./fastrack.sh -usrdump   : Generate the userdump on the log folder                              *"
-  echo "*  ./fastrack.sh -heapdump  : Generate the heapdump on the log folder                              *"
-  echo "*  ./fastrack.sh -traceon [DEBUG,INFO,WARN,ERROR,FATAL,INFO,TRACE] : Update traces severity        *"
-  echo "*  ./fastrack.sh -traceoff  : Deactivate the traces                                                *"
-  echo "*  ./fastrack.sh -listmac   : List machine mac address                                             *"
-  echo "*  ./fastrack.sh -stop      : Stops application                                                    *"
+  echo "*  ./fastboot.sh -start     : Starts the application                                               *"
+  echo "*  ./fastboot.sh -stats     : Get application state and queues list                                *"
+  echo "*  ./fastboot.sh -refresh   : Reload the application configuration from SAP and restart the queue  *"
+  echo "*  ./fastboot.sh -version   : Get application version                                              *"
+  echo "*  ./fastboot.sh -usrdump   : Generate the userdump on the log folder                              *"
+  echo "*  ./fastboot.sh -heapdump  : Generate the heapdump on the log folder                              *"
+  echo "*  ./fastboot.sh -traceon [DEBUG,INFO,WARN,ERROR,FATAL,INFO,TRACE] : Update traces severity        *"
+  echo "*  ./fastboot.sh -traceoff  : Deactivate the traces                                                *"
+  echo "*  ./fastboot.sh -listmac   : List machine mac address                                             *"
+  echo "*  ./fastboot.sh -stop      : Stops application                                                    *"
   echo "****************************************************************************************************"
 
 elif [ "$1" = "-start" ]; then
   echo "Starting application"
-  java -Xms2048m -Xmx2048m -Dapp.dir="$APP_DIR" -cp "$CLASSPATH" com.primeur.fastrack.core.Fastrack
+  java -Xms2048m -Xmx2048m -Dapp.dir="$APP_DIR" -cp "$CLASSPATH" com.bamboolab.fastboot.core.fastboot
 elif [ "$1" = "-stats" ] || [ "$1" = "-traceon" ] || [ "$1" = "-traceof" ] || [ "$1" = "-stop" ] || [ "$1" = "-usrdump" ] || [ "$1" = "-heapdump" ] || [ "$1" = "-refresh" ]; then
-  echo "Calling Fastrack service"
-  java -Dapp.dir="$APP_DIR" -cp "$CLASSPATH" com.primeur.fastrack.rmi.Client "$@"
+  echo "Calling fastboot service"
+  java -Dapp.dir="$APP_DIR" -cp "$CLASSPATH" com.bamboolab.fastboot.rmi.Client "$@"
 elif  [ "$1" = "-version" ] || [ "$1" = "-listmac" ]; then
-  echo "Calling Fastrack utils"
-  java -Dapp.dir="$APP_DIR" -cp "$CLASSPATH" com.primeur.fastrack.core.Client "$@"
+  echo "Calling fastboot utils"
+  java -Dapp.dir="$APP_DIR" -cp "$CLASSPATH" com.bamboolab.fastboot.core.Client "$@"
 fi
 
